c3e5205015672a9b8426000e270ad3de49a31ee0
_M.Gh0st_ <140098387+M1sterGh0sT@users.noreply.github.com>
2024-07-27 13:32:22 +0200

    add functions selecting.py
    add all function which described in README.md file

--- selecting.py
+++ selecting.py
@@ -1,50 +1,188 @@
-# Selecting Sort
-
-# more clear variant
-
-
-def selectingSort_1(arr):
-    result_arr = []
-    for j in range(len(arr)):
-        smallest_num = arr.index(min(arr))  # find index of the smallest number in array
-        result_arr.append(arr.pop(smallest_num))
-
-    return result_arr
-
-
-def selectingSort_2(arr):
-    result_arr = []
-    for j in range(len(arr)):
-        result_arr.append(arr.pop(arr.index(min(arr))))
-    return result_arr
-
-
-
-print(selectingSort_1([9, 2, 4, 1, 0, 5, 6]))
-print(selectingSort_2([9, 2, 4, 1, 0, 5, 6]))
-
-
-
-
-# Quick Sort
-
-def quickSort(arr):
-    if len(arr) < 2:
-        return arr
-    else:
-        pivot = arr[0]
-        less = [j for j in arr[1::] if j <= pivot]
-        greater = [j for j in arr[1::] if j > pivot]
-    return quickSort(less) + [pivot] + quickSort(greater)
-
-print(quickSort([9, 2, 4, 1, 0, 5, 6]))
-
-
-
-# BogoSort
-import random
-def bogoSort(arr):
-    while not sorted(arr) == arr:
-        random.shuffle(arr)
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
-print(bogoSort([9, 2, 4, 1, 0, 5, 6]))
+
+
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key
+    return arr
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+    return arr
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, 0):
+        heapify(arr, n, i)
+
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+    return arr
+
+
+def shell_sort(arr):
+    n = len(arr)
+    gap = n // 2
+
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
+
+
+def counting_sort(arr):
+    max_val = max(arr)
+    m = max_val + 1
+    count = [0] * m
+
+    for a in arr:
+        count[a] += 1
+
+    i = 0
+    for a in range(m):
+        for c in range(count[a]):
+            arr[i] = a
+            i += 1
+    return arr
+
+
+def counting_sort_for_radix(arr, exp):
+    n = len(arr)
+    output = [0] * n
+    count = [0] * 10
+
+    for i in range(n):
+        index = arr[i] // exp
+        count[index % 10] += 1
+
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+
+    i = n - 1
+    while i >= 0:
+        index = arr[i] // exp
+        output[count[index % 10] - 1] = arr[i]
+        count[index % 10] -= 1
+        i -= 1
+
+    for i in range(len(arr)):
+        arr[i] = output[i]
+
+def radix_sort(arr):
+    max_val = max(arr)
+    exp = 1
+    while max_val // exp > 0:
+        counting_sort_for_radix(arr, exp)
+        exp *= 10
+    return arr
+
+
+
+def bucket_sort(arr):
+    max_val = max(arr)
+    size = max_val / len(arr)
+
+    buckets = [[] for _ in range(len(arr))]
+
+    for i in range(len(arr)):
+        j = int(arr[i] / size)
+        if j != len(arr):
+            buckets[j].append(arr[i])
+        else:
+            buckets[len(arr) - 1].append(arr[i])
+
+    for i in range(len(arr)):
+        buckets[i] = sorted(buckets[i])
+
+    result = []
+    for i in range(len(arr)):
+        result.extend(buckets[i])
+
+    return result
+
